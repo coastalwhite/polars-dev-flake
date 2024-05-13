33c8ae622089c4fd57ac9249953b01c1a106681c
coastalwhite <me@gburghoorn.com>
2024-05-13 21:14:56 +0200

    Working flake with nice aliases

--- flake.nix
+++ flake.nix
@@ -16,12 +16,104 @@
           inherit system;
           overlays = [ rust-overlay.overlays.default ];
         };
-
-        polarsPath = "$HOME/Projects/polars";
+        lib = pkgs.lib;
 
         rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        python = (pkgs.python3.withPackages ( python-pkgs: let
+          localPyPkg = file: import file {
+            python = pkgs.python3;
+            python-pkgs = python-pkgs;
+            pkgs = pkgs;
+            buildPythonPackage = python-pkgs.buildPythonPackage;
+            fetchPypi = python-pkgs.fetchPypi;
+          };
+        in with python-pkgs; [
+          hypothesis
+          pytest
+          (localPyPkg ./python-packages/pytest-codspeed.nix)
+          pytest-cov
+          pytest-xdist
+
+          numpy
+          pandas
+          pyarrow
+
+          # backports-zoneinfo
+          tzdata
+
+          sqlalchemy
+          (localPyPkg ./python-packages/adbc-driver-manager.nix)
+          (localPyPkg ./python-packages/adbc-driver-sqlite.nix)
+          aiosqlite
+          (localPyPkg ./python-packages/connectorx.nix)
+          (localPyPkg ./python-packages/kuzu.nix)
+
+          cloudpickle
+          fsspec
+          s3fs
+          
+          lxml
+          openpyxl
+          pyxlsb
+          xlsx2csv
+          XlsxWriter
+          (localPyPkg ./python-packages/deltalake.nix)
+
+          zstandard
+          hvplot
+          matplotlib
+          gevent
+          nest-asyncio
+        ]));
       in {
-        devShells.default = pkgs.mkShell {
+        devShells.default = let
+					aliases = rec {
+						check = {
+							cmd = "cargo check --workspace --all-targets --all-features";
+							doc = "Run cargo check with all features";
+						};
+						clippy-all = {
+							cmd = "cargo clippy --workspace --all-targets --all-features --locked -- -D warnings -D clippy::dbg_macro";
+							doc = "Run clippy with all features";
+						};
+						clippy-default = {
+							cmd = "cargo clippy --all-targets --locked -- -D warnings -D clippy::dbg_macro";
+							doc = "Run clippy with default features";
+						};
+						fmt = {
+							cmd = "cargo fmt --all";
+							doc = "Run autoformatting";
+						};
+						pybuild = {
+							cmd = "maturin develop -m $POLARS_ROOT/py-polars/Cargo.toml";
+							doc = "Build the python library";
+						};
+						pytest = {
+							cmd = "pytest -n auto --dist loadgroup";
+							doc = "Run the default python tests";
+						};
+						precommit = {
+							cmd = ''
+                set -e
+								echo '[Format]'
+								${fmt.cmd}
+								echo 'Format Done ✅'
+                echo
+								echo '[Clippy All]:'
+								${clippy-all.cmd}
+								echo 'Clippy All Done ✅'
+                echo
+								echo '[Clippy Default]:'
+								${clippy-default.cmd}
+								echo 'Clippy Default Done ✅'
+							'';
+							doc = "Run the checks to do before committing";
+						};
+					};
+
+          mapAttrsToList = lib.attrsets.mapAttrsToList;
+        in pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             maturin
             rustToolchain
@@ -29,61 +121,37 @@
             typos
             mypy
             dprint
+
+            zlib
             
+            python
             fix-python.packages.${system}.default
-            
-            (python3.withPackages ( python-pkgs: let
-              localPyPkg = file: import file {
-                python = python3;
-                python-pkgs = python-pkgs;
-                pkgs = pkgs;
-                buildPythonPackage = python-pkgs.buildPythonPackage;
-                fetchPypi = python-pkgs.fetchPypi;
-              };
-              
-            in with python-pkgs; [
-              hypothesis
-              pytest
-              (localPyPkg ./python-packages/pytest-codspeed.nix)
-              pytest-cov
-              pytest-xdist
+          ] ++ (
+            mapAttrsToList (name: value: pkgs.writeShellScriptBin "pl-${name}" value.cmd) aliases
+          );
 
-              numpy
-              pandas
-              pyarrow
+          shellHook = let
+						concatStrings = lib.concatStrings;
 
-              # backports-zoneinfo
-              tzdata
-
-              sqlalchemy
-              (localPyPkg ./python-packages/adbc-driver-manager.nix)
-              (localPyPkg ./python-packages/adbc-driver-sqlite.nix)
-              aiosqlite
-              (localPyPkg ./python-packages/connectorx.nix)
-              (localPyPkg ./python-packages/kuzu.nix)
-
-              cloudpickle
-              fsspec
-              s3fs
-              
-              lxml
-              openpyxl
-              pyxlsb
-              xlsx2csv
-              XlsxWriter
-              (localPyPkg ./python-packages/deltalake.nix)
-
-              zstandard
-              hvplot
-              matplotlib
-              gevent
-              nest-asyncio
-            ]))
-          ];
-
-          shellHook = ''
+						max = x: y:
+                assert builtins.isInt x;
+                assert builtins.isInt y;
+                if x < y then y
+                         else x;
+            listMax = lib.foldr max 0;
+            maxLength = listMax (mapAttrsToList (name: _: (builtins.stringLength name)) aliases);
+            nSpaces = n: (lib.concatMapStrings (_: " ") (lib.range 1 n));
+					in ''
             echo 'Welcome in your Polars Development Environment :)' | ${pkgs.lolcat}/bin/lolcat
-            export PYTHONPATH="$PYTHONPATH:${polarsPath}/py-polars"
+						export POLARS_ROOT="$HOME/Projects/polars"
+            export PYTHONPATH="$PYTHONPATH:$POLARS_ROOT/py-polars"
+
+						echo
+						echo ${builtins.toString (maxLength)}
+						echo 'Defined Aliases:'
+						${concatStrings (mapAttrsToList (name: value: ''
+							echo ' - pl-${name}:${nSpaces (maxLength - (builtins.stringLength name))} ${value.doc}'
+						'') aliases)}
           '';
         };
       }
