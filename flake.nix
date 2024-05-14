7c28b8a8456359bea4af1ed75e25414a05d29eff
coastalwhite <me@gburghoorn.com>
2024-05-14 10:41:23 +0200

    retab, add additional dependencies, better pytest

--- flake.nix
+++ flake.nix
@@ -29,11 +29,19 @@
             fetchPypi = python-pkgs.fetchPypi;
           };
         in with python-pkgs; [
+          pydantic
           hypothesis
           pytest
           (localPyPkg ./python-packages/pytest-codspeed.nix)
           pytest-cov
           pytest-xdist
+
+          flask
+          flask-cors
+
+          moto
+          boto3
+          importlib-resources
 
           numpy
           pandas
@@ -68,49 +76,62 @@
         ]));
       in {
         devShells.default = let
-					aliases = rec {
-						check = {
-							cmd = "cargo check --workspace --all-targets --all-features";
-							doc = "Run cargo check with all features";
-						};
-						clippy-all = {
-							cmd = "cargo clippy --workspace --all-targets --all-features --locked -- -D warnings -D clippy::dbg_macro";
-							doc = "Run clippy with all features";
-						};
-						clippy-default = {
-							cmd = "cargo clippy --all-targets --locked -- -D warnings -D clippy::dbg_macro";
-							doc = "Run clippy with default features";
-						};
-						fmt = {
-							cmd = "cargo fmt --all";
-							doc = "Run autoformatting";
-						};
-						pybuild = {
-							cmd = "maturin develop -m $POLARS_ROOT/py-polars/Cargo.toml";
-							doc = "Build the python library";
-						};
-						pytest = {
-							cmd = "pytest -n auto --dist loadgroup";
-							doc = "Run the default python tests";
-						};
-						precommit = {
-							cmd = ''
+          aliases = rec {
+            check = {
+              cmd = "cargo check --workspace --all-targets --all-features";
+              doc = "Run cargo check with all features";
+            };
+            clippy-all = {
+              cmd = "cargo clippy --workspace --all-targets --all-features --locked -- -D warnings -D clippy::dbg_macro";
+              doc = "Run clippy with all features";
+            };
+            clippy-default = {
+              cmd = "cargo clippy --all-targets --locked -- -D warnings -D clippy::dbg_macro";
+              doc = "Run clippy with default features";
+            };
+            fmt = {
+              cmd = "cargo fmt --all";
+              doc = "Run autoformatting";
+            };
+            pybuild = {
+              cmd = "maturin develop -m $POLARS_ROOT/py-polars/Cargo.toml";
+              doc = "Build the python library";
+            };
+            pytest-all = {
+              cmd = "pytest -n auto --dist loadgroup \"$@\"";
+              doc = "Run the default python tests";
+            };
+            pytest = {
+              cmd = "pytest \"$@\"";
+              doc = "Run the default python tests";
+            };
+            pyfmt = {
+              cmd = ''
+                ruff check
+                ruff format
+                dprint fmt
+                typos
+              '';
+              doc = "Run python autoformatting";
+            };
+            precommit = {
+              cmd = ''
                 set -e
-								echo '[Format]'
-								${fmt.cmd}
-								echo 'Format Done ✅'
+                echo '[Format]'
+                ${fmt.cmd}
+                echo 'Format Done ✅'
                 echo
-								echo '[Clippy All]:'
-								${clippy-all.cmd}
-								echo 'Clippy All Done ✅'
+                echo '[Clippy All]:'
+                ${clippy-all.cmd}
+                echo 'Clippy All Done ✅'
                 echo
-								echo '[Clippy Default]:'
-								${clippy-default.cmd}
-								echo 'Clippy Default Done ✅'
-							'';
-							doc = "Run the checks to do before committing";
-						};
-					};
+                echo '[Clippy Default]:'
+                ${clippy-default.cmd}
+                echo 'Clippy Default Done ✅'
+              '';
+              doc = "Run the checks to do before committing";
+            };
+          };
 
           mapAttrsToList = lib.attrsets.mapAttrsToList;
         in pkgs.mkShell {
@@ -131,9 +152,9 @@
           );
 
           shellHook = let
-						concatStrings = lib.concatStrings;
+            concatStrings = lib.concatStrings;
 
-						max = x: y:
+            max = x: y:
                 assert builtins.isInt x;
                 assert builtins.isInt y;
                 if x < y then y
@@ -141,17 +162,17 @@
             listMax = lib.foldr max 0;
             maxLength = listMax (mapAttrsToList (name: _: (builtins.stringLength name)) aliases);
             nSpaces = n: (lib.concatMapStrings (_: " ") (lib.range 1 n));
-					in ''
+          in ''
             echo 'Welcome in your Polars Development Environment :)' | ${pkgs.lolcat}/bin/lolcat
-						export POLARS_ROOT="$HOME/Projects/polars"
+            export POLARS_ROOT="$HOME/Projects/polars"
             export PYTHONPATH="$PYTHONPATH:$POLARS_ROOT/py-polars"
 
-						echo
-						echo ${builtins.toString (maxLength)}
-						echo 'Defined Aliases:'
-						${concatStrings (mapAttrsToList (name: value: ''
-							echo ' - pl-${name}:${nSpaces (maxLength - (builtins.stringLength name))} ${value.doc}'
-						'') aliases)}
+            echo
+            echo ${builtins.toString (maxLength)}
+            echo 'Defined Aliases:'
+            ${concatStrings (mapAttrsToList (name: value: ''
+              echo ' - pl-${name}:${nSpaces (maxLength - (builtins.stringLength name))} ${value.doc}'
+            '') aliases)}
           '';
         };
       }
